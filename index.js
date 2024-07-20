808f40575cecd5e490d9354110c35ea600062e07
Saad Suleman <91990781+SAADSULEMAN053@users.noreply.github.com>
2024-07-20 22:01:19 +0500

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,56 @@ const productRoutes = require('./routes/productRoutes');
 const userRoutes = require('./routes/userRoutes');
 app.use(express.json());
 
+app.get('/toplawyers', (req,res)=>
+    {
+        const TopLawyersContent = [
+            {
+              id:1,
+              LawyerName: "Sarah Smith",
+              Category:"Business & Corporate Law",
+              Rate:"$200/hr",
+            },
+            {
+              id:2,
+              LawyerName:"John Lee",
+              Category: "Legal Consultation on Intellectual Property",
+              Rate:"$200/hr",
+            },
+            {
+              id:3,
+              LawyerName:"Emily Chen",
+              Category: "Taxation & Financial Law",
+              Rate:"$200/hr",
+            },
+            {
+              id:4,
+              LawyerName:"David Patel",
+              Category:"Environmental and Energy Law",
+              Rate:"$200/hr",
+            },
+            {
+              id:5,
+              LawyerName:"Sarah Smith",
+              Category: "Environmental and Energy Law",
+              Rate:"$200/hr",
+            },
+            {
+              id:6,
+              LawyerName:"Emily Chen",
+              Category: "Environmental and Energy Law",
+              Rate:"$200/hr",
+            },
+            {
+              id:7,
+              LawyerName:"Sarah Smith",
+              Category: "Environmental and Energy Law",
+              Rate:"$200/hr",
+            },
+          ]; 
+        res.send(TopLawyersContent);
+    });
+
+
 app.get('/', (req, res) => {
     res.send('products api running new deploy');
 });
@@ -14,7 +64,7 @@ app.get('/ping', (req, res) => {
     res.send('<=PONG=>');
 });
 app.get('/country', (req, res) => {
-    res.send('<=INDIA=>');
+    res.send('<=PAK=>');
 });
 
 app.get('/ping', (req, res) => {
